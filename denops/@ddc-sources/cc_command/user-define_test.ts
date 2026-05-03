96aaedcba8319f663641ac59094892a2c54dac1a
Omochice <mochice.mls.ntl@gmail.com>
2026-05-04 03:52:45 +0900

    test(user-define): add failing test for reading a command file
    A markdown file under <configDir>/commands/ should be returned as a slash
    item with word '/<basename>' and info from the frontmatter description.

--- denops/@ddc-sources/cc_command/user-define_test.ts
+++ denops/@ddc-sources/cc_command/user-define_test.ts
@@ -2,13 +2,36 @@ import { assertEquals } from "jsr:@std/assert@1";
 import { join } from "jsr:@std/path@1";
 import { collectGlobal } from "./user-define.ts";
 
-Deno.test("collectGlobal returns [] when configDir does not exist", async () => {
-  const tmp = await Deno.makeTempDir();
+async function withTempConfigDir(
+  fn: (configDir: string) => Promise<void>,
+): Promise<void> {
+  const configDir = await Deno.makeTempDir();
   try {
+    await fn(configDir);
+  } finally {
+    await Deno.remove(configDir, { recursive: true });
+  }
+}
+
+Deno.test("collectGlobal returns [] when configDir does not exist", async () => {
+  await withTempConfigDir(async (tmp) => {
     const missing = join(tmp, "does-not-exist");
     const items = await collectGlobal(missing);
     assertEquals(items, []);
-  } finally {
-    await Deno.remove(tmp, { recursive: true });
-  }
+  });
+});
+
+Deno.test("collectGlobal reads a command file as a slash item", async () => {
+  await withTempConfigDir(async (configDir) => {
+    const commandsDir = join(configDir, "commands");
+    await Deno.mkdir(commandsDir, { recursive: true });
+    await Deno.writeTextFile(
+      join(commandsDir, "foo.md"),
+      "---\ndescription: Do the foo thing\n---\nbody\n",
+    );
+
+    const items = await collectGlobal(configDir);
+
+    assertEquals(items, [{ word: "/foo", info: "Do the foo thing" }]);
+  });
 });
